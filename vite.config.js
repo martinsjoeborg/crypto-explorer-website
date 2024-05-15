9b336014dff7e8ec8ba6d4f40257a23e6428a785
martinsjoeborg <113284827+martinsjoeborg@users.noreply.github.com>
2024-05-15 21:50:03 +0200

    ye

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/crypto-explorer-website/"
+  base: "/crypto-explorer-website/",
+  resolve: {
+    alias: {
+      // Add any necessary aliases here
+    },
+    mainFields: ['module', 'main'], // Ensure module field is prioritized
+  },
 })
