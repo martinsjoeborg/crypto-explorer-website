8a44e68ef2cddeae7bc83057646cd80d633436b8
martinsjoeborg <113284827+martinsjoeborg@users.noreply.github.com>
2024-05-15 21:53:12 +0200

    try4

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/crypto-explorer-website/",
-  resolve: {
-    alias: {
-      // Add any necessary aliases here
-    },
-    mainFields: ['module', 'main'], // Ensure module field is prioritized
-  },
+  plugins: [
+    react(),
+    nodePolyfills()
+  ],
+  base: "/crypto-explorer-website/"
+  
 })
